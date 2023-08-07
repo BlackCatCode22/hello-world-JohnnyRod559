73608a144809b108b2119f48808b561afbb51cd7
JohnnyRod559 <141672746+JohnnyRod559@users.noreply.github.com>
2023-08-07 11:49:38 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\n\n\n\n Hello world! \n\n" << endl;
+    cout << "\n\n Hello from the first day of class \n\n" << endl;
+    return 0;
+}
